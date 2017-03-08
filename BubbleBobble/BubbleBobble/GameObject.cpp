2d57d40a818b190bad9ccf1026ce57c93354b92e
William Eiffert <weiffert@gmail.com>
2017-03-08 16:01:59 -0600

    Fleshed out basic time and distance functions

--- BubbleBobble/BubbleBobble/GameObject.cpp
+++ BubbleBobble/BubbleBobble/GameObject.cpp
@@ -1,12 +1,131 @@
 #include "stdafx.h"
 #include "GameObject.h"
+#include <math.h>
 
 
 GameObject::GameObject()
 {
+	//add instance variables
 }
 
 
 GameObject::~GameObject()
 {
+}
+
+void GameObject::update()
+{
+	if (trackingTime)
+	{
+		time();
+	}
+	if (trackingDistance)
+	{
+		distance();
+	}
+}
+
+//Game Logic
+void GameObject::collision(GameObject)
+{
+	//if collision, call collided();
+}
+
+//Returns true and stops the timer 
+void GameObject::time()
+{
+	if (clock.getElapsedTime() > timeLimit)
+	{
+		stopClock();
+		timeLimitPassed();
+	}
+}
+
+/*Updates pedometer to be the absolute value of the distance traveled along the 
+hypotnuse from the velocity.*/
+void GameObject::distance()
+{
+	pedometer += std::abs(std::sqrt(velocity.x * velocity.x + velocity.y * velocity.y));
+	
+	if (pedometer > distanceLimit)
+	{
+		stopPedometer();
+		distanceLimitPassed();
+	}
+}
+
+//Specific event type functions.
+void GameObject::collided()
+{
+
+}
+void GameObject::death()
+{
+
+}
+void GameObject::timeLimitPassed()
+{
+
+}
+void GameObject::distanceLimitPassed()
+{
+
+}
+
+//get variables.
+std::string GameObject::getName()
+{
+	return name;
+}
+sf::Vector2f GameObject::getVelocity()
+{
+	return velocity;
+}
+sf::Time GameObject::getTimeElapsed()
+{
+	if (trackingTime)
+		return clock.getElapsedTime();
+	return timeElapsed;
+}
+float GameObject::getDistanceElapsed()
+{
+	return pedometer;
+}
+
+
+//Set variables
+void GameObject::setName(std::string set)
+{
+	name = set;
+}
+void GameObject::setVelocity(sf::Vector2f set)
+{
+	velocity = set;
+}
+void GameObject::setTimeLimit(sf::Time set)
+{
+	timeLimit = set;
+}
+void GameObject::setPedometerLimit(float set)
+{
+	distanceLimit = set;
+}
+void GameObject::startClock()
+{
+	trackingTime = true;
+	clock.restart();
+}
+void GameObject::startPedometer()
+{
+	trackingDistance = true;
+	pedometer = 0;
+}
+double GameObject::stopClock()
+{
+	trackingTime = false;
+	timeElapsed = clock.getElapsedTime();
+}
+double GameObject::stopPedometer()
+{
+	trackingDistance = false;
 }
--- BubbleBobble/BubbleBobble/GameObject.h
+++ BubbleBobble/BubbleBobble/GameObject.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "SFML\System.hpp"
+#include "SFML\Graphics.hpp"
 
 class GameObject
 {
@@ -9,21 +11,51 @@ public:
 	~GameObject();
 	//Game Logic
 	void collision(GameObject);
+	void time();
+	void distance();
+
 	//Updates every step.
 	virtual void update();
+
 	//Specific event type functions.
-	virtual void collide();
+	virtual void collided();
+	virtual void timeLimitPassed();
+	virtual void distanceLimitPassed();
 	virtual void death();
-	virtual void startTimer();
-	virtual void startPedometer();
-	virtual void stopTimer();
-	virtual void stopPedometer();
+
+	std::string getName();
+	sf::Time getTimeElapsed();
+	float getDistanceElapsed();
+	sf::Vector2f getVelocity();
+	//need to be added
+	sf::RectangleShape getRectangle();
+	sf::Texture getTexture();
+
+	void setName(std::string);
+	void setTimeLimit(sf::Time);
+	void setPedometerLimit(float);
+	void setVelocity(sf::Vector2f);
+
+	void startClock();
+	void startPedometer();
+	double stopClock();
+	double stopPedometer();
 
 protected:
-	double velocityX;
-	double velocityY;
-	double timer;
-	double pedometer;
+	sf::Vector2f velocity;
+
+	sf::Clock clock;
+	sf::Time timeElapsed;
+	sf::Time timeLimit;
+	bool trackingTime;
+
+	float pedometer;
+	float distanceElapsed;
+	float distanceLimit;
+	bool trackingDistance;
+
+	sf::RectangleShape rectangle;
+	sf::Texture texture;
 	std::string name;
 	std::vector<double> data;
 };
